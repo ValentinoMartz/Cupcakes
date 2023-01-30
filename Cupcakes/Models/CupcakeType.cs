5b0c06e612032bb51d1bfa1dd7af47de4e722792
ValentinoMartz <valentinomartinez55@gmail.com>
2023-01-30 20:56:14 -0300

    Agregar archivos de proyecto.

---
+++ Cupcakes/Models/CupcakeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cupcakes.Models
+{
+    public enum CupcakeType
+    {
+        Birthday,
+        Turquoise,
+        Chocolate,
+        Strawberry
+    }
+}
